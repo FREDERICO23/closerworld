9738ee955b6a0307640cb64692f7cf3b906a0628
KweCita <agooogwang92@gmail.com>
2022-07-28 11:39:02 +0300

    added files

---
+++ Closerworld/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Closerworld project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Closerworld.settings')
+
+application = get_wsgi_application()
---
+++ static/script.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll(".one");
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    removeActiveClasses();
+    item.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  items.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
+
